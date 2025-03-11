4f22bd1c87c9d86058e74dd6c5cfb6ba33d6884a
harshithuetech <harshit.huetech@gmail.com>
2025-03-11 23:06:23 +0530

    Data Base change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-const Database_URL = "mongodb+srv://root:root@cluster0.ca2cg9i.mongodb.net/Blossobit?retryWrites=true&w=majority&appName=Cluster0";
+const Database_URL = "mongodb://127.0.0.1:27017/KICTCMS?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.4.2";
 
 mongoose.connect(Database_URL).then(()=>console.log('!!MongoDB Connected!!')).catch((err)=> console.log(err.message));
